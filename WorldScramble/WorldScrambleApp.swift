444186d7b390c316fbc6319619033cd5041c962d
hugosilvag6 <hugosilvag6@hotmail.com>
2022-07-09 22:23:14 -0300

    Initial Commit

---
+++ WorldScramble/WorldScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldScrambleApp.swift
+//  WorldScramble
+//
+//  Created by Hugo Silva on 09/07/22.
+//
+
+import SwiftUI
+
+@main
+struct WorldScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
